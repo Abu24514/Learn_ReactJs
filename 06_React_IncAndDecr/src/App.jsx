f00c8118e258efa7d8a6222327d811dad8cc8b19
Abu Hanjala <abuhanjala656@gmail.com>
2026-04-30 10:22:07 +0530

    best use of useState

---
+++ 06_React_IncAndDecr/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+/* best practice of useState */
+const App = () => {
+  const [num, setNum] = useState(0);
+
+  const increaseNum = () => {
+    setNum(num + 1);
+  };
+  const decreaseNum = () => {
+    setNum(num - 1);
+  };
+  const jumpNum = () => {
+    setNum(num + 5);
+  };
+  const reset = ()=>{
+    setNum(0);
+  }
+  return (
+    <>
+      <div className="main">
+        <div className="card">
+          <h1>{num}</h1>
+          <div className="butn">
+            <button id="btn" className="incr" onClick={increaseNum}>
+              Increase
+            </button>
+            <button id="btn" className="decr" onClick={decreaseNum}>
+              Decrease
+            </button>
+            <button id="btn" className="jump" onClick={jumpNum}>
+              JumpBy5
+            </button>
+            <button className="reset" onClick={reset}>Reset</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
